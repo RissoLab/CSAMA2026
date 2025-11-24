c60c2f7a193f4076f61e63015054a9ac4250889c
Davide Risso <risso.davide@gmail.com>
2025-11-24 12:28:05 +0100

    Initial commit

---
+++ installation_script/linux_libraries.sh
@@ -0,0 +1,26 @@
+## Installation of required Ubuntu libraries for installing CSAMA R packages
+## tested on Ubuntu 22.04, you may need to change some library names for older versions
+
+sudo apt-get install build-essential \
+libxml2-dev \
+libcurl4-openssl-dev \
+libcairo2-dev \
+libreadline-dev \
+libjpeg-dev \
+libtiff5-dev \
+libpng-dev \
+libssl-dev \
+libssh2-1-dev \
+libhunspell-dev \
+libnetcdf-dev \
+libpq-dev \
+libglu1-mesa-dev \
+libxt-dev \
+libudunits2-dev \
+libgdal-dev \
+libgsl-dev \
+default-jdk \
+netcdf-bin \
+libbz2-dev \
+libsasl2-dev \
+libfftw3-dev
